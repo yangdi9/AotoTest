50f64ac57d70de578fbfc93f7d2795f464ad98a4
Your Name <you@example.com>
2018-06-09 10:06:36 +0800

    上传代码

---
+++ DemoOne/src/main/java/com/source/testng/suite/payTest.java
@@ -0,0 +1,10 @@
+package com.source.testng.suite;
+
+import org.testng.annotations.Test;
+
+public class payTest {
+    @Test
+    public void payzhifubao(){
+        System.out.println("支付宝支付成功");
+    }
+}
